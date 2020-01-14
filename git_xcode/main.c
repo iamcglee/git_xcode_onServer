cb49f208802ee130f6b261651b6f248af31cee99
Chang-Gun Lee <iamcglee@gmail.com>
2020-01-14 16:51:51 +0900

    Initial Commit

---
+++ git_xcode/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  git_xcode
+//
+//  Created by Chang-Gun Lee on 2020. 1. 14..
+//  Copyright © 2020년 Chang-Gun Lee. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
